900189e0a5a85950f7c9d7903ab70cefa0d815ce
Ouma <tonouma@amazon.com>
2019-04-17 20:22:27 -0400

    finish shopping cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ɵcontainerRefreshStart} from '@angular/core';
+import { APIService} from '../API.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  total = 0;
+
+  cart: any = [];
+
+  constructor(private api: APIService, private router: Router) { }
 
   ngOnInit() {
+    this.api.ListCarts().then(data => {
+      this.cart = data.items;
+      console.log(this.cart);
+    } )
+      .catch(err => console.log(err));
+  }
+  onDelete(item) {
+    /*const payload = {id: item.id, owner: item.owner,
+    price: item.price, sku: item.sku, product: item.product, __typename: item.__typename}*/
+    const payload = {id: item.id};
+    this.api.DeleteCart(payload)
+      .then(data => {
+        console.log(data);
+        this.ngOnInit();
+      });
+    console.log('Delete item from the cart: ' + item.id);
+
   }
 
+  getTotal(): number {
+    return this.cart.map(t => t.price).reduce((acc, value) => acc + value, 0);
+  }
+
+  checkOut(item) {
+    this.router.navigateByUrl('/order');
+  }
 }
+
